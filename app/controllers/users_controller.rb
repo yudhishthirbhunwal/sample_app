6b08e884a53dcba7a237a5d34451127c85122060
Yudhishthir <yudhishthir.it21@gmail.com>
2021-04-09 13:24:27 +0530

    [Add user profile page]:  -Add show page and action for user profile  -Add routes for user resources  -Add debug box and its css  -Add gravatar_for helper

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,23 @@
 class UsersController < ApplicationController
+  def show
+    @user=User.find(params[:id])
+  end
+
   def new
+  end
+  
+  def create
+  end
+
+  def edit
+    
+  end
+
+  def update
+    
+  end
+
+  def destroy
+    
   end
 end
