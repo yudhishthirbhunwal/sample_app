c4247e6332f082464ed0b154f52cc1848dff6242
Yudhishthir <yudhishthir.it21@gmail.com>
2021-04-14 19:01:37 +0530

    [Updating users]:  -Add edit form for updating users  -Refactor edit and new views to create a form partial  -Add test for successful and unsuccessful attempts for updating  -Add required authorization for preventing unauthorized access  -Add friendly forwarding feature

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,8 @@
 class UsersController < ApplicationController
+
+  before_action :logged_in_user, only: [:edit, :update]
+  before_action :correct_user, only: [:edit, :update]
+
   def index
     @users=User.all
   end
@@ -23,11 +27,17 @@ class UsersController < ApplicationController
   end
 
   def edit
-    
+    @user = User.find(params[:id])
   end
 
   def update
-    
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
   end
 
   def destroy
@@ -39,5 +49,22 @@ class UsersController < ApplicationController
     def user_params
       params.require(:user).permit(:name, :email, :password,
                                     :password_confirmation)
+    end
+
+    # Before filters
+
+    # Confirms a logged-in user.
+    def logged_in_user
+      unless logged_in?
+        store_location
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
+    end
+
+    # Confirms the correct user.
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
     end
 end
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,8 +1,27 @@
 require "test_helper"
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @user = users(:michael)
+    @other_user = users(:archer)
+  end
+
   test "should get new" do
     get signup_path
     assert_response :success
+  end
+
+  test "should redirect edit when logged in as wrong user" do
+    get edit_user_path(@user)
+    assert_not flash.empty?
+    assert_redirected_to login_url
+  end
+
+  test "should redirect update when logged in as wrong user" do
+    patch user_path(@user), params: { user: { name: @user.name,
+                                              email: @user.email } }
+    assert_not flash.empty?
+    assert_redirected_to login_url
   end
 end
