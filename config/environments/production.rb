3209026f81686349bfa1f431170465221435c42b
Yudhishthir <yudhishthir.it21@gmail.com>
2021-04-15 19:00:32 +0530

    [Minor Fix #3]:  -Minor fix in production environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,7 +70,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   host = 'fierce-fortress-22209.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
-  ActionMailer::Base.smtp_settings = {
+  config.action_mailer.smtp_settings = {
   :address => 'smtp.gmail.com',
   :port => '587',
   :authentication => :plain,
