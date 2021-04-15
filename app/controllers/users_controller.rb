4e68d4cd955e7eb6a5832a6ca4c687c413a23879
Yudhishthir <yudhishthir.it21@gmail.com>
2021-04-15 11:52:09 +0530

    [Delete Users]:  -Add destroy action to delete users  -Add admin rights for admin users  -Set admin to true in fixtures and seeds  -Add test cases for proper authorization for deleting users  -Add delete link to users index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
 
-  before_action :logged_in_user, only: [:index, :edit, :update]
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
   before_action :correct_user, only: [:edit, :update]
+  before_action :admin_user, only: :destroy
 
   def index
     @users = User.paginate(page: params[:page])
@@ -41,14 +42,16 @@ class UsersController < ApplicationController
   end
 
   def destroy
-    
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted"
+    redirect_to users_url
   end
 
   private
 
     def user_params
       params.require(:user).permit(:name, :email, :password,
-                                    :password_confirmation)
+                                    :password_confirmation  )
     end
 
     # Before filters
@@ -66,5 +69,10 @@ class UsersController < ApplicationController
     def correct_user
       @user = User.find(params[:id])
       redirect_to(root_url) unless current_user?(@user)
+    end
+
+    # Confirms an admin user.
+    def admin_user
+      redirect_to(root_url) unless current_user.admin?
     end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -10,7 +10,8 @@
 User.create!( name: "Example User",
               email: "example@railstutorial.org",
               password: "foobar",
-              password_confirmation: "foobar")
+              password_confirmation: "foobar",
+              admin: true )
 
 # Generate a bunch of additional users.
 99.times do |n|
@@ -20,5 +21,5 @@ User.create!( name: "Example User",
   User.create!( name: name,
                 email: email,
                 password: password,
-                password_confirmation: password)
+                password_confirmation: password )
 end
