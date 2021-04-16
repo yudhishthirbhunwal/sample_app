e6a7a56617fc15c3394b6923644e1d9f96d6d82d
Yudhishthir <yudhishthir.it21@gmail.com>
2021-04-16 13:37:32 +0530

    [Add Password Reset]:  -Add update action in password resets controller  -Add integration test for password resets  -Add edit view for password resets

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -2,15 +2,16 @@ class PasswordResetsController < ApplicationController
 
   before_action :get_user, only: [:edit, :update]
   before_action :valid_user, only: [:edit, :update]
+  before_action :check_expiration, only: [:edit, :update]
 
   def new
   end
 
   def create
+    @user = User.find_by(email: params[:password_reset][:email].downcase)
     if @user
-      @user = User.find_by(email: params[:password_reset][:email].downcase)
       @user.create_reset_digest
-      @user.send_password_reset_mail
+      @user.send_password_reset_email
       flash[:info] = "Email sent with password reset instructions"
       redirect_to root_url
     else
@@ -22,7 +23,27 @@ class PasswordResetsController < ApplicationController
   def edit
   end
 
+  def update
+    if params[:user][:password].empty?
+      @user.errors.add(:password, "can't be empty")
+      render 'edit'
+    elsif @user.update(user_params)
+      log_in @user
+      @user.update_attribute(:reset_digest, nil)
+      flash[:success] = "Password has been reset"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
   private
+
+    def user_params
+      params.require(:user).permit(:password, :password_confirmation)
+    end
+
+    # Before Filters
 
     def get_user
       @user = User.find_by(email: params[:email])
@@ -33,6 +54,14 @@ class PasswordResetsController < ApplicationController
       unless (@user && @user.activated? &&
               @user.authenticated?(:reset, params[:id]))
       redirect_to root_url
+      end
+    end
+
+    # Checks expiration of reset token.
+    def check_expiration
+      if @user.password_reset_expired?
+        flash[:danger] = "Password reset has expired."
+        redirect_to new_password_reset_url
       end
     end
 
