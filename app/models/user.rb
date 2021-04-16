d9eaa3057880a00aa928914964232b9acf41b79d
Yudhishthir <yudhishthir.it21@gmail.com>
2021-04-16 12:25:37 +0530

    [Password Reset]:  -Add password reset controller  -Add views for password reset mailer  -Add password reset attributes to user model  -Add test for password reset

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
 
-  attr_accessor :remember_token, :activation_token
+  attr_accessor :remember_token, :activation_token, :reset_token
 
   before_save :downcase_email
   before_create :create_activation_digest
@@ -28,11 +28,11 @@ class User < ApplicationRecord
     end
   end
 
-    # Remembers a user in the database for use in persistent sessions.
-    def remember
-      self.remember_token = User.new_token
-      update_attribute(:remember_digest, User.digest(remember_token))
-    end
+  # Remembers a user in the database for use in persistent sessions.
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
 
   # Returns true if the given token matches the digest.
   def authenticated?(attribute, token)
@@ -54,6 +54,17 @@ class User < ApplicationRecord
   # Sends activation email.
   def send_activation_email
     UserMailer.account_activation(self).deliver_now
+  end
+
+  # Sets the password reset attributes.
+  def create_reset_digest
+    self.reset_token = User.new_token
+    update_columns(reset_digest: User.digest(reset_token), reset_sent_at: Time.zone.now)
+  end
+
+  # Sends password reset email.
+  def send_password_reset_email
+    UserMailer.password_reset(self).deliver_now
   end
 
   private
